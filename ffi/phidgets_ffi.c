24e26be48095a5658d49de6a2f87a23b57ca7bcf
andrew36b <andrew36b@38b188ea-9f4b-0410-b627-fd7e04d4b4ed>
2008-06-22 18:02:22 +0000

    removed prints from ffi calls

--- ffi/phidgets_ffi.c
+++ ffi/phidgets_ffi.c
@@ -18,15 +18,12 @@ uint32_t *phidgetsOpen( uint32_t context_len, uint32_t *stackvar )
   rc = CPhidgetTextLCD_create( &phid ); 
   assert( rc == 0 );
   assert( (uint32_t) phid != 0 );
-  printf("got past create\n");
 
-  rc = CPhidget_open( (CPhidgetHandle) phid, -1 /* 68132 */ /*serial_number*/ );
+  rc = CPhidget_open( (CPhidgetHandle) phid, serial_number );
   assert( rc == 0 );
-  printf("got past open\n");
 
   rc = CPhidget_waitForAttachment( (CPhidgetHandle) phid, 0 );
   assert( rc == 0 );
-  printf("got past attachment\n");
  
   return boxUnsigned( context_len, (uint32_t) phid);
 }
@@ -52,6 +49,8 @@ uint32_t *phidgetsSetDisplay( uint32_t context_len, uint32_t *tuple )
 {
   int rc;
   char phidget_str[PHIDGETS_MAX_CHARS + 1];
+  char phidget_line1[PHIDGETS_MAX_CHARS / 2 + 1];
+  char phidget_line2[PHIDGETS_MAX_CHARS / 2 + 1];
   CPhidgetTextLCDHandle phid;
 
   phid = (CPhidgetTextLCDHandle) unboxUnsigned( unboxTuple(tuple, 0) );
@@ -61,12 +60,15 @@ uint32_t *phidgetsSetDisplay( uint32_t context_len, uint32_t *tuple )
 
   if (strlen(phidget_str) > PHIDGETS_MAX_CHARS / 2)
   {
-    rc = CPhidgetTextLCD_setDisplayString(phid, 0, phidget_str);
-    rc = CPhidgetTextLCD_setDisplayString(phid, 1, phidget_str + PHIDGETS_MAX_CHARS / 2);
+    strncpy(phidget_line1, phidget_str, PHIDGETS_MAX_CHARS / 2);
+    rc = CPhidgetTextLCD_setDisplayString(phid, 0, phidget_line1);
+    strncpy(phidget_line2, phidget_str + PHIDGETS_MAX_CHARS / 2, PHIDGETS_MAX_CHARS / 2);
+    rc = CPhidgetTextLCD_setDisplayString(phid, 1, phidget_line2);
   }
   else
   {
-    rc = CPhidgetTextLCD_setDisplayString(phid, 0, phidget_str);
+    strncpy(phidget_line1, phidget_str, PHIDGETS_MAX_CHARS / 2);
+    rc = CPhidgetTextLCD_setDisplayString(phid, 0, phidget_line1);
   }
 
   return (uint32_t *) NULL;
