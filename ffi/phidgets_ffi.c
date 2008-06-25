8ee1e14f29ad3b908af7c56eb7defa9a687b30b5
andrew36b <andrew36b@38b188ea-9f4b-0410-b627-fd7e04d4b4ed>
2008-06-25 03:44:02 +0000

    added output state and analog input functions

--- ffi/phidgets_ffi.c
+++ ffi/phidgets_ffi.c
@@ -7,7 +7,7 @@
 
 #define PHIDGETS_MAX_CHARS 20
 
-uint32_t *phidgetsOpen( uint32_t context_len, uint32_t *stackvar )
+uint32_t *phidgetsLCDOpen( uint32_t context_len, uint32_t *stackvar )
 {
   int rc;
   CPhidgetTextLCDHandle phid;
@@ -16,6 +16,27 @@ uint32_t *phidgetsOpen( uint32_t context_len, uint32_t *stackvar )
   serial_number = (int32_t) unboxUnsigned( stackvar );
 
   rc = CPhidgetTextLCD_create( &phid ); 
+  assert( rc == 0 );
+  assert( (uint32_t) phid != 0 );
+
+  rc = CPhidget_open( (CPhidgetHandle) phid, serial_number );
+  assert( rc == 0 );
+
+  rc = CPhidget_waitForAttachment( (CPhidgetHandle) phid, 0 );
+  assert( rc == 0 );
+ 
+  return boxUnsigned( context_len, (uint32_t) phid);
+}
+
+uint32_t *phidgetsIKOpen( uint32_t context_len, uint32_t *stackvar )
+{
+  int rc;
+  CPhidgetInterfaceKitHandle phid;
+  int32_t serial_number;
+
+  serial_number = (int32_t) unboxUnsigned( stackvar );
+
+  rc = CPhidgetInterfaceKit_create( &phid ); 
   assert( rc == 0 );
   assert( (uint32_t) phid != 0 );
 
@@ -56,6 +77,7 @@ uint32_t *phidgetsSetDisplay( uint32_t context_len, uint32_t *tuple )
   assert( phid != 0 );
 
   lineno = (int) unboxUnsigned( unboxTuple(tuple, 1) );
+  assert( (lineno >= 0) && (lineno < 2) ); 
 
   memset(phidget_str, 0x0, sizeof(phidget_str));
 
@@ -66,3 +88,65 @@ uint32_t *phidgetsSetDisplay( uint32_t context_len, uint32_t *tuple )
 
   return (uint32_t *) NULL;
 }
+
+uint32_t *phidgetsGetSensorRawValue( uint32_t context_len, uint32_t *tuple )
+{
+  int rc;
+  int index;
+  int raw_value;
+  CPhidgetInterfaceKitHandle phid;
+
+  phid = (CPhidgetInterfaceKitHandle) unboxUnsigned( unboxTuple(tuple, 0) );
+  assert( phid != 0 );
+
+  index = (int) unboxUnsigned( unboxTuple(tuple, 1) );
+  assert( (index >= 0) && (index < 8) );
+
+  rc = CPhidgetInterfaceKit_getSensorRawValue( phid, index, &raw_value );
+  assert( rc == 0 );
+
+  return boxUnsigned( context_len, (uint32_t) raw_value );
+}
+
+uint32_t *phidgetsGetOutputState( uint32_t context_len, uint32_t *tuple )
+{
+  int rc;
+  int index;
+  int output_state;
+  CPhidgetInterfaceKitHandle phid;
+
+  phid = (CPhidgetInterfaceKitHandle) unboxUnsigned( unboxTuple(tuple, 0) );
+  assert( phid != 0 );
+
+  index = (int) unboxUnsigned( unboxTuple(tuple, 1) );
+  assert( (index >= 0) && (index < 8) );
+
+  rc = CPhidgetInterfaceKit_getOutputState( phid, index, &output_state );
+  assert( rc == 0 );
+
+  return boxUnsigned( context_len, (uint32_t) output_state );
+}
+
+uint32_t *phidgetsSetOutputState( uint32_t context_len, uint32_t *tuple )
+{
+  int rc;
+  int index;
+  int desired_value;
+  CPhidgetInterfaceKitHandle phid;
+
+  phid = (CPhidgetInterfaceKitHandle) unboxUnsigned( unboxTuple(tuple, 0) );
+  assert( phid != 0 );
+
+  index = (int) unboxUnsigned( unboxTuple(tuple, 1) );
+  assert( (index >= 0) && (index < 8) );
+
+  desired_value = (int) unboxUnsigned( unboxTuple(tuple, 2) );
+  assert( (desired_value == 0) || (desired_value == 1) );
+
+  rc = CPhidgetInterfaceKit_setOutputState( phid, index, desired_value );
+  printf("desired value = %d\n", desired_value);
+  assert( rc == 0 );
+
+  return (uint32_t *) NULL;
+}
+
