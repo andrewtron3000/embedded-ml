aadcfde978b8b425079629dfc02e77aef7f3cf37
andrew36b <andrew36b@38b188ea-9f4b-0410-b627-fd7e04d4b4ed>
2008-07-31 23:08:12 +0000

    added server socket support

--- ffi/socket_ffi.c
+++ ffi/socket_ffi.c
@@ -1,3 +1,10 @@
+/*
+ * socket_ffi.c
+ *
+ * Some code taken from Ivan Griffin (ivan.griffin@ul.ie)
+ *
+ */
+
 #include <stdio.h>                /* perror() */
 #include <stdlib.h>               /* atoi() */
 #include <string.h>
@@ -9,6 +16,7 @@
 #include <netdb.h>
 #include <assert.h>
 #include <inttypes.h>
+#include <fcntl.h>
 
 #include "ffi.h"
 
@@ -17,6 +25,8 @@
 uint32_t *socketOpen( uint32_t context_len, uint32_t *stackvar )
 {
   int sd;
+  int status;
+  int enabled = 1;
 
   sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (-1 == sd)
@@ -24,8 +34,36 @@ uint32_t *socketOpen( uint32_t context_len, uint32_t *stackvar )
     perror("socket:");
     exit(1);
   }
+
+  status = setsockopt(sd, 
+                      SOL_SOCKET,
+                      SO_REUSEADDR,
+                      (const char *) &enabled, 
+                      sizeof(enabled));
+
+  if (-1 == status)
+  {
+    perror("setsockopt(...,SO_REUSEADDR,...)");
+  }
  
+  /* SUSP -- add the NODELAY one too */
+
   return boxUnsigned( context_len, (uint32_t) sd);
+}
+
+uint32_t *socketSetNonBlocking(uint32_t context_len, uint32_t *hPtr)
+{
+  int sd;
+  int flags;
+
+  sd = (int) unboxUnsigned( hPtr );
+
+  flags = fcntl(sd, F_GETFL, 0);
+  assert( flags >= 0 );
+
+  fcntl(sd, F_SETFL, flags | O_NONBLOCK);
+
+  return (uint32_t *) NULL;
 }
 
 uint32_t *socketConnect(uint32_t context_len, uint32_t *tuple )
@@ -39,7 +77,7 @@ uint32_t *socketConnect(uint32_t context_len, uint32_t *tuple )
 
   sd = (int) unboxUnsigned( unboxTuple(tuple, 0) );
 
-  unboxString( unboxTuple(tuple, 1), hostname, sizeof(hostname) );
+  unboxString( unboxTuple(tuple, 1), hostname, HOSTNAME_LEN );
 
   port = (int) unboxUnsigned( unboxTuple(tuple, 2) );
 
@@ -76,3 +114,86 @@ uint32_t *socketConnect(uint32_t context_len, uint32_t *tuple )
   return (uint32_t *) NULL;
 }
 
+uint32_t *socketBind(uint32_t context_len, uint32_t *tuple )
+{
+  int sd;
+  int port;
+  int status = 0;
+  struct hostent *hostPtr = NULL;
+  struct sockaddr_in serverName = { 0 };
+
+  sd = (int) unboxUnsigned( unboxTuple(tuple, 0) );
+  port = (int) unboxUnsigned( unboxTuple(tuple, 1) );
+
+  hostPtr = gethostbyname("127.0.0.1");
+  if (NULL == hostPtr)
+  {
+    perror("gethostbyname()");
+    exit(1);
+  }
+
+  (void) memset(&serverName, 0,
+                sizeof(serverName));
+  (void) memcpy(&serverName.sin_addr,
+                hostPtr->h_addr,
+                hostPtr->h_length);
+
+  serverName.sin_addr.s_addr=htonl(INADDR_ANY);
+  serverName.sin_family = AF_INET;
+  serverName.sin_port = htons(port);
+
+  status = bind(sd,
+                (struct sockaddr *) &serverName,
+                sizeof(serverName));
+  if (-1 == status)
+  {
+    perror("bind()");
+    exit(1);
+  }
+
+  return (uint32_t *) NULL;
+}
+
+uint32_t *socketListen(uint32_t context_len, uint32_t *tuple )
+{
+  int sd;
+  int n;
+  int status;
+
+  sd = (int) unboxUnsigned( unboxTuple(tuple, 0) );
+  n = (int) unboxUnsigned( unboxTuple(tuple, 1) );
+
+  status = listen(sd, n);
+  if (-1 == status)
+  {
+    perror("listen()");
+    exit(1);
+  }
+
+  return (uint32_t *) NULL;
+}
+
+uint32_t *socketAccept(uint32_t context_len, uint32_t *hptr )
+{
+  int sd;
+  int new_sd;
+  struct sockaddr_in clientName = { 0 };
+  int clientLength = sizeof(clientName);
+
+  sd = (int) unboxUnsigned( hptr );
+
+  (void) memset(&clientName, 0,
+                sizeof(clientName));
+
+  new_sd = accept(sd,
+                  (struct sockaddr *) &clientName,
+                  (uint32_t *) &clientLength);
+
+  if (-1 == new_sd)
+  {
+    perror("accept()");
+    exit(1);
+  }
+
+  return boxUnsigned( context_len, new_sd );
+}
