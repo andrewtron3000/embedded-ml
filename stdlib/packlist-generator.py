8b78fffeb76761e2c3827ebf2001eb4b906c3ade
andrew36b <andrew36b@38b188ea-9f4b-0410-b627-fd7e04d4b4ed>
2008-08-10 16:54:14 +0000

    new addition of packlist utility

---
+++ stdlib/packlist-generator.py
@@ -0,0 +1,52 @@
+import sys
+
+# packlist file format
+# type name
+# recordname size
+# ...
+
+if len(sys.argv) < 2:
+    print 'Usage: %s filename' % sys.argv[0]
+    exit(1)
+
+def printRecord(rec, others, typ, siz):
+    s = '('
+    s = s + 'fn r => #%s/%s r,\n' % (rec, typ)
+    s = s + ' (fn r v => { %s = v,\n' % rec
+    num_others = len(others)
+    for (i, o) in enumerate(others):
+        if rec != o:
+            s = s + '              %s = #%s/%s r' % (o, o, typ)
+            if i < num_others - 1:
+                s = s + ',\n'
+            else:
+                s = s + '}),\n'
+    s = s + (' %s)' % siz)
+    return s
+
+
+filename = sys.argv[1]
+
+f = open(sys.argv[1])
+ls = f.readlines()
+num_record_elements = len(ls) - 1
+
+typename = ls[0].strip()
+records = []
+sizes = []
+
+for l in ls[1:]:
+    (r, s) = l.split()
+    records.append(r.strip())
+    sizes.append(s.strip())
+
+print 'val %s-packlist =' % typename
+num_records = len(ls) - 1
+for (i, r) in enumerate(records):
+    print printRecord(r, records, typename, sizes[i]),
+    if i < num_records - 1:
+        print '::\n'
+    else:
+        print ':: nil\n'
+        
+
